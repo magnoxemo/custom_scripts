d0a007b040292ec6180c747ad6cc07f03ab5d980
magnoxemo <walid.ahammed01@gmail.com>
2025-01-24 12:37:26 -0600

    adding eigen include

--- libmesh_stuff/run_libmesh_code.py
+++ libmesh_stuff/run_libmesh_code.py
@@ -7,11 +7,14 @@ _parser.add_argument("--exe", help=" name of the cpp exe file ")
 
 _libmesh_include_path = "/home/ebny_walid/libmesh_opt/include"
 _libmesh_link_path = "/home/ebny_walid/libmesh_opt/lib"
+_eigen_path ="/usr/include/eigen3"
 
 _args = _parser.parse_args()
+
 _run_command = (
     f"mpicxx {_args.cpp} -o {_args.exe} "
     f"-I{_libmesh_include_path} "
+    f"-I{_eigen_path} "
     f"-Wl,-rpath,{_libmesh_link_path} "
     f"-L{_libmesh_link_path} "
     f"-lmesh_opt -ltimpi_opt -lnglib -lngcore -lz -ltirpc "
@@ -22,3 +25,4 @@ _run_command = (
 _executable_command = "./" +_args.exe
 os.system(_run_command)
 os.system(_executable_command)
+
