d0158950e33c6a75cac998b27fad259b10fd648a
magnoxemo <walid.ahammed01@gmail.com>
2025-02-02 22:21:53 -0600

    adding debugger mode

--- libmesh_stuff/run_libmesh_code.py
+++ libmesh_stuff/run_libmesh_code.py
@@ -5,6 +5,7 @@ _parser = argparse.ArgumentParser()
 _parser.add_argument("--cpp", help = " name of the cpp src file ")
 _parser.add_argument("--exe", help = " name of the cpp exe file ")
 _parser.add_argument("--rebuild", help = "If the program needs to be rebuild",default = False)
+_parser.add_argument("--dbg",help = " Debugger mode",default = False)
 
 _libmesh_include_path = "/home/ebny_walid/libmesh_opt/include"
 _libmesh_link_path = "/home/ebny_walid/libmesh_opt/lib"
@@ -12,18 +13,30 @@ _eigen_path ="/usr/include/eigen3"
 
 _args = _parser.parse_args()
 
-_run_command = (
-    f"mpicxx {_args.cpp} -o {_args.exe} "
-    f"-I{_libmesh_include_path} "
-    f"-I{_eigen_path} "
-    f"-Wl,-rpath,{_libmesh_link_path} "
-    f"-L{_libmesh_link_path} "
-    f"-lmesh_opt -ltimpi_opt -lnglib -lngcore -lz -ltirpc "
-    f"-O2 -felide-constructors -fstrict-aliasing -Wdisabled-optimization "
-    f"-funroll-loops -ftrapping-math -fopenmp"
-)
-
-_executable_command = "./" +_args.exe
+if _args.dbg:
+    _run_command = (
+        f"mpicxx {_args.cpp} -g -oo -o {_args.exe} "
+        f"-I{_libmesh_include_path} "
+        f"-I{_eigen_path} "
+        f"-Wl,-rpath,{_libmesh_link_path} "
+        f"-L{_libmesh_link_path} "
+        f"-lmesh_opt -ltimpi_opt -lnglib -lngcore -lz -ltirpc "
+        f"-O2 -felide-constructors -fstrict-aliasing -Wdisabled-optimization "
+        f"-funroll-loops -ftrapping-math -fopenmp"
+    )
+    _executable_command = "gdb ./" +_args.exe
+else:
+    _run_command = (
+        f"mpicxx {_args.cpp} -o {_args.exe} "
+        f"-I{_libmesh_include_path} "
+        f"-I{_eigen_path} "
+        f"-Wl,-rpath,{_libmesh_link_path} "
+        f"-L{_libmesh_link_path} "
+        f"-lmesh_opt -ltimpi_opt -lnglib -lngcore -lz -ltirpc "
+        f"-O2 -felide-constructors -fstrict-aliasing -Wdisabled-optimization "
+        f"-funroll-loops -ftrapping-math -fopenmp"
+    )
+    _executable_command = "./" +_args.exe
 
 if os.path.exists(_args.exe) and _args.rebuild :
 
@@ -42,7 +55,7 @@ else:
         os.system(_executable_command)
     else:
         print( "===================================================")
-        print(f"=====  {_args.exe} doesn't exists so building it   =======")
+        print(f"===== {_args.exe} doesn't exists so building it  =======")
         print( "===================================================")
         os.system(_run_command)
         if os.path.exists(_args.exe):
@@ -50,4 +63,3 @@ else:
             os.system(_executable_command)
         else:
             raise Exception (f"\n\n\n{_args.cpp} file didn't compile. See the compile message and fix them.")
-
