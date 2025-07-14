0d944d7114393b8983c4849bc4d95242c4e50647
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2025-07-14 12:39:12 -0500

    adding discrepancy calculator

---
+++ cardinal_stuff/DiscrepancyCalculator.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas
+import scipy
+import matplotlib.pyplot as plt
+import numpy
+from matplotlib import cm
+
+
+class MeshData:
+    def __init__(self, data_file_path: str, variable_name: str):
+        try:
+            self.data_file = pandas.read_csv(data_file_path)
+        except ValueError as error:
+            raise Exception(f"{data_file_path} not found. Throwing {error}.")
+
+        self.variable_name = variable_name
+        self.x_coordinate = numpy.array(self.data_file['x'])
+        self.y_coordinate = numpy.array(self.data_file['y'])
+        self.z_coordinate = numpy.array(self.data_file['z'])
+        self.metric_variable_data = numpy.array(self.data_file[self.variable_name])
+
+    def interpolate(self):
+        interpolator = scipy.interpolate.RBFInterpolator(
+            numpy.column_stack((self.x_coordinate, self.y_coordinate, self.z_coordinate)),
+            self.metric_variable_data)
+        return interpolator
+
+
+class CalculateDiscrepancyMatrix:
+    def __init__(self, amr: MeshData, amr_with_mesh_amalgamation: MeshData):
+        self.amr = amr
+        self.amr_with_mesh_amalgamation = amr_with_mesh_amalgamation
+        self.amr_rbf = self.amr.interpolate()  # amr mapping
+        self.amr_with_mesh_amalgamation_rbf = self.amr_with_mesh_amalgamation.interpolate()  # mesh amalgamation mapping
+
+    def get_discrepancy_matrix(self, x, y, z):
+        amr = self.amr_rbf(numpy.column_stack((x, y, z)))
+        mesh_amalgamation = self.amr_with_mesh_amalgamation_rbf(numpy.column_stack((x, y, z)))
+        return (amr - mesh_amalgamation) / amr
+
+    def plot_data(self, n_points=1000):
+        cmap = cm.viridis
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection='3d')
+
+        # random points in the domain
+        x_ = np.random.uniform(numpy.min(self.amr.x_coordinate), numpy.max(self.amr.x_coordinate), n_points)
+        y_ = np.random.uniform(numpy.min(self.amr.y_coordinate), numpy.max(self.amr.y_coordinate), n_points)
+        z_ = np.random.uniform(numpy.min(self.amr.z_coordinate), numpy.max(self.amr.z_coordinate), n_points)
+        value = self.get_discrepancy_matrix(x_, y_, z_)
+        ax.scatter(x_, y_, z_, facecolors=cm.viridis(value), linewidth=0)
+
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_zlabel('Z-axis')
+
+        m = cm.ScalarMappable(cmap=cmap)
+        m.set_array(value)
+        fig.colorbar(m, shrink=0.5, aspect=10, label='Value (Val)')
+        plt.show()
