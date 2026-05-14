a637ed1680e647a9a7e4cc65b99c9f032f1199e6
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2026-05-14 10:41:33 -0500

    add env variable to fix the MPI err
    Updated SLURM settings and script commands for Cardinal.

--- cardinal_stuff/submit_script.sh
+++ cardinal_stuff/submit_script.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-#SBATCH --partition=shared
+#SBATCH --partition=pre
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=2000
-#SBATCH --time=0-48:00:00
+#SBATCH --time=0-4:00:00
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
 
 module load openmpi
-
-export image_path=/scratch/eahammed/
-export bind_path=/scratch/eahammed/cardinal_amr
-export cross_sections=/scratch/eahammed/cross_sections/endfb-viii.0-hdf5
+export UCX_POSIX_USE_PROC_LINK=n
+export image_path=/scratch/eahammed/software/cardinal_dev/cardinal.sif
+export bind_path=$PWD
+export cross_sections=/scratch/eahammed/cross_sections/endfb-viii.0-hdf5/
+TOTAL_THREADS=$(( SLURM_NTASKS_PER_NODE * SLURM_CPUS_PER_TASK ))
 
 srun apptainer exec \
     --bind ${bind_path}:${bind_path} \
     --bind ${cross_sections}:${cross_sections} \
-    ${image_path}/cardinal_on_hpc.sif \
+    ${image_path} \
     bash -c "export OPENMC_CROSS_SECTIONS=${cross_sections}/cross_sections.xml && \
-             cd ${bind_path}/models/sfr/assembly && \
-             /opt/cardinal-build/cardinal/cardinal-opt -i openmc.i --n-threads=2 > logfile.\${SLURM_PROCID}.txt 2>&1"
+             cd ${bind_path} && \
+             /opt/cardinal-build/cardinal/cardinal-opt -i 3D_bpf_unit_cells.i --mesh-only --n-threads=${TOTAL_THREADS} && \
+             /opt/cardinal-build/cardinal/cardinal-opt -i openmc.i --n-threads=${TOTAL_THREADS}"
