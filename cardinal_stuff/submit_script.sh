39ea4b20d42d54dd5799fb2285440f041b39c52d
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2026-09-13 13:50:14 -0500

    Update SLURM script for Cardinal job configuration

--- cardinal_stuff/submit_script.sh
+++ cardinal_stuff/submit_script.sh
@@ -1,25 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --partition=pre
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=64
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2000
-#SBATCH --time=0-4:00:00
-#SBATCH --error=job.%J.err
-#SBATCH --output=job.%J.out
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem-per-cpu=0
+#SBATCH --time=0-24:00:00
+#SBATCH --job-name=3x3_20_axial_2_radial
+#SBATCH --error=10_axial_1_radial.%J.err
+#SBATCH --output=10_axial_1_radial.%J.out
+
+# Usage: sbatch multiphysics_amr.sh [recover] --> skip mesh regeneration, resume Cardinal from the latest checkpoint
+RECOVER=${1:-0}
+
+N_THREADS=16
 
 module load openmpi
 export UCX_POSIX_USE_PROC_LINK=n
+
+export cross_sections=/scratch/eahammed/cross_sections/
 export image_path=/scratch/eahammed/software/cardinal_dev/cardinal.sif
-export bind_path=$PWD
-export cross_sections=/scratch/eahammed/cross_sections/endfb-viii.0-hdf5/
-TOTAL_THREADS=$(( SLURM_NTASKS_PER_NODE * SLURM_CPUS_PER_TASK ))
+
+export bind_path="/scratch/eahammed/amr_test_cases_input_files/multi_physics_amr/"
+export input_path=${PWD}
+
+CARDINAL=/opt/cardinal-build/cardinal/cardinal-opt
+
+if [[ "${RECOVER}" == "recover" ]]; then
+  RUN_CMD="${CARDINAL} -i openmc.i --n-threads=${N_THREADS} --recover"
+else
+  RUN_CMD="${CARDINAL} -i mesh_neutronics.i --mesh-only --n-threads=${N_THREADS} && \
+  ${CARDINAL} -i mesh_hc.i --mesh-only --n-threads=${N_THREADS} && \
+  ${CARDINAL} -i openmc.i --n-threads=${N_THREADS}"
+fi
 
 srun apptainer exec \
-    --bind ${bind_path}:${bind_path} \
-    --bind ${cross_sections}:${cross_sections} \
-    ${image_path} \
-    bash -c "export OPENMC_CROSS_SECTIONS=${cross_sections}/cross_sections.xml && \
-             cd ${bind_path} && \
-             /opt/cardinal-build/cardinal/cardinal-opt -i 3D_bpf_unit_cells.i --mesh-only --n-threads=${TOTAL_THREADS} && \
-             /opt/cardinal-build/cardinal/cardinal-opt -i openmc.i --n-threads=${TOTAL_THREADS}"
+  --bind ${bind_path}:${bind_path} \
+  --bind ${cross_sections}:${cross_sections} \
+  ${image_path} bash -c "export OPENMC_CROSS_SECTIONS=${cross_sections}/endfb-viii.0-hdf5/cross_sections.xml && \
+  cd ${input_path} && \
+  ${RUN_CMD}"
